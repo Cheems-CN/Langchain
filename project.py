b95c70037cad5dae128fd17d43107e3c89853f9a
sen <2974556560@qq.com>
2025-01-22 12:09:15 +0800

    记录历史

--- project.py
+++ project.py
@@ -9,26 +9,15 @@ import  langchain_community
 
 model = ChatOllama(model='qwen2.5:latest')
 parser = StrOutputParser()
-prompt = ChatPromptTemplate([
-
-    ('system', 'you are a teacher teaching math'),
-    ('ai', 'hello, whats your problem about this'),
-    ('user', '{content}.Can you teach me some?')
-])
-
-chain = prompt| model | parser
 
 
-app = fastapi.FastAPI(title='''sen's serve''', version='V1.0.0', description='miss u')
-langserve.add_routes(
 
-    app,
-    chain,
-    path="/senmissu"
 
+p = PromptTemplate(
+    input_variables=['target'],
+    template='你好，介绍一下{target}'
 )
+chain = p| model | parser
+print(chain.invoke({'target':'自己'}))
 
-if __name__ == "__main__":
-    import uvicorn
 
-    uvicorn.run(app, host="localhost", port=8000)
